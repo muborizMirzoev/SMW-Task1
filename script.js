1b11d69db42662271ffab77dd1e6128e204566a6
muborizMirzoev <muboriz10@gmail.com>
2020-03-29 22:02:06 +0700

    feat: add scroll-down and scroll-up

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// Back-to-Top
+function backToTop() {
+  const button = $('.back-to-top');
+
+  // $(window).on('scroll', () => {
+  //   if ($(this).scrollTop() >= 4000) {
+  //     button.fadeIn();
+  //   } else {
+  //     button.fadeOut();
+  //   }
+  // });
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop: 0}, 1000);
+  }); 
+
+}
+backToTop();
+
+
+
+//Scroll Down
+$(function() {
+  $('.scroll-down').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
